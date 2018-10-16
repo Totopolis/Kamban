ee1a027bc49fdb6a87e92bfe74ce59b998fe0630
Totopolis <anikeew@gmail.com>
2018-10-16 18:35:38 +0300

    Exporter implemented

--- KambanSolution/Kamban/ViewModels/ExportViewModel.cs
+++ KambanSolution/Kamban/ViewModels/ExportViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Kamban.Model;
+using Newtonsoft.Json;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Ui.Wpf.Common;
 using Ui.Wpf.Common.ViewModels;
@@ -15,6 +18,14 @@ namespace Kamban.ViewModels
     {
         [Reactive] public BoardInfo Board { get; set; }
         [Reactive] public bool IsChecked { get; set; }
+    }
+
+    public class DatabaseToExport
+    {
+        public List<BoardInfo> BoardList { get; set; } = new List<BoardInfo>();
+        public List<ColumnInfo> ColumnList { get; set; } = new List<ColumnInfo>();
+        public List<RowInfo> RowList { get; set; } = new List<RowInfo>();
+        public List<Issue> IssueList { get; set; } = new List<Issue>();
     }
 
     public class ExportViewModel : ViewModelBase, IInitializableViewModel
@@ -50,7 +61,7 @@ namespace Kamban.ViewModels
                 (sf, br) => !string.IsNullOrEmpty(sf) && Boards.Where(z => z.IsChecked).Count() > 0);
 
             SelectTargetFolderCommand = ReactiveCommand.Create(SelectTargetFolderCommandExecute);
-            ExportCommand = ReactiveCommand.Create(ExportCommandExecute);
+            ExportCommand = ReactiveCommand.CreateFromTask(ExportCommandExecute);
             CancelCommand = ReactiveCommand.Create(() => this.Close());
 
             // TODO: handle not found file
@@ -77,9 +88,99 @@ namespace Kamban.ViewModels
                 });
         }
 
-        private void ExportCommandExecute()
+        private async Task ExportCommandExecute()
         {
+            string fileName = TargetFolder + "\\" + TargetFile;
+            if (DatePostfix)
+                fileName += "_" + DateTime.Now.ToString("yyyyMMdd");
 
+            // TODO: check target folder exists
+            if (!SplitBoardsToFiles)
+                await DoExportWhole(fileName);
+            else
+                await DoExportSplit(fileName);
+        }
+
+        private async Task DoExportWhole(string fileName)
+        {
+            // 1. prepare database
+            var jb = new DatabaseToExport();
+            jb.BoardList.AddRange(await prjService.GetAllBoardsInFileAsync());
+
+            foreach (var brd in jb.BoardList)
+            {
+                var columns = await prjService.GetColumnsByBoardIdAsync(brd.Id);
+                jb.ColumnList.AddRange(columns);
+
+                var rows = await prjService.GetRowsByBoardIdAsync(brd.Id);
+                jb.RowList.AddRange(rows);
+
+                var issues = await prjService.GetIssuesByBoardIdAsync(brd.Id);
+                jb.IssueList.AddRange(issues);
+            }
+
+            // 2. export
+            DoExportForNeededFormats(jb, fileName);
+        }
+
+        private async Task DoExportSplit(string fileName)
+        {
+            var boards = await prjService.GetAllBoardsInFileAsync();
+
+            foreach (var brd in boards)
+            {
+                // 1. prepare database
+                var jb = new DatabaseToExport();
+                jb.BoardList.Add(brd);
+
+                var columns = await prjService.GetColumnsByBoardIdAsync(brd.Id);
+                jb.ColumnList.AddRange(columns);
+                var rows = await prjService.GetRowsByBoardIdAsync(brd.Id);
+                jb.RowList.AddRange(rows);
+                var issues = await prjService.GetIssuesByBoardIdAsync(brd.Id);
+                jb.IssueList.AddRange(issues);
+
+                // 2. export
+                DoExportForNeededFormats(jb, fileName + "_" + brd.Name);
+            }
+        }
+
+        private void DoExportForNeededFormats(DatabaseToExport db, string fileName)
+        {
+            if (ExportJson)
+                DoExportJson(db, fileName + ".json");
+
+            if (ExportKamban)
+                DoExportKamban(db, fileName + ".db");
+        }
+
+        private void DoExportJson(DatabaseToExport db, string fileName)
+        {
+            // TODO: check target file not exists
+
+            string output = JsonConvert.SerializeObject(db, Formatting.Indented);
+            File.WriteAllText(fileName, output);
+        }
+
+        private void DoExportKamban(DatabaseToExport db, string fileName)
+        {
+            // TODO: check target file not exists
+
+            var prj = appModel.CreateProjectService(fileName);
+
+            foreach (var brd in db.BoardList)
+            {
+                prj.CreateOrUpdateBoardAsync(brd);
+
+                foreach (var col in db.ColumnList)
+                    prj.CreateOrUpdateColumnAsync(col);
+
+                foreach (var row in db.RowList)
+                    prj.CreateOrUpdateRowAsync(row);
+
+                foreach (var iss in db.IssueList)
+                    prj.CreateOrUpdateIssueAsync(iss);
+            }
         }
 
         private void SelectTargetFolderCommandExecute()
