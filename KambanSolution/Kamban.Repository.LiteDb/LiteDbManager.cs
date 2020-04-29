a21658b1e03eb3905e8395a40b731998a0fa1424
Konstantin Anikeev <anikeew@gmail.com>
2020-04-30 00:18:26 +0300

    litedb transaction log file fix

---
+++ KambanSolution/Kamban.Repository.LiteDb/LiteDbManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace Kamban.Repository.LiteDb
+{
+    public class LiteDbManager
+    {
+        private readonly ConnectionString connStr;
+        private LiteDatabase dba;
+        private int counter;
+
+        public LiteDbManager(string uri)
+        {
+            dba = null;
+            counter = 0;
+
+            connStr = new ConnectionString()
+            {
+                Filename = uri,
+                Upgrade = true
+            };
+
+            // prepare db
+            try
+            {
+                var db = LockDb();
+
+                db.CheckpointSize = 0;
+                db.Checkpoint();
+                db.Rebuild();
+            }
+            finally
+            {
+                FreeDb();
+            }
+        }
+
+        public LiteDatabase LockDb()
+        {
+            lock(this)
+            {
+                if (dba == null)
+                    dba = new LiteDatabase(connStr);
+
+                counter++;
+
+                return dba;
+            }
+        }
+
+        public void FreeDb()
+        {
+            lock (this)
+            {
+                if (counter > 0)
+                    counter--;
+
+                // set an killers timer
+                if (counter == 0)
+                    Task.Delay(2000)
+                        .ContinueWith(_ => CloseDb());
+
+                if (counter < 0)
+                    throw new Exception("Abnormal counter behavior");
+            }
+        }
+
+        public void CloseDb() 
+        {
+            lock(this)
+            {
+                if (counter == 0 && dba != null)
+                {
+                    dba.Checkpoint();
+                    dba.Dispose();
+                    dba = null;
+                }
+            }
+        }
+
+    }//end of class
+}
--- KambanSolution/Kamban.Repository.LiteDb/LiteDbRepository.cs
+++ KambanSolution/Kamban.Repository.LiteDb/LiteDbRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Kamban.Contracts;
@@ -8,145 +9,266 @@ namespace Kamban.Repository.LiteDb
 {
     public class LiteDbRepository : IRepository
     {
-        private readonly LiteDatabase db;
+        private readonly LiteDbManager manager;
 
         public LiteDbRepository(string uri)
         {
-            var connStr = new ConnectionString()
-            {
-                Filename = uri,
-                Upgrade = true
-            };
-
-            db = new LiteDatabase(connStr);
-        }
-
-        public async Task<Box> Load()
-        {
-            var cards = db.GetAllAsync<Card>();
-            var rows = db.GetAllAsync<Row>();
-            var columns = db.GetAllAsync<Column>();
-            var boards = db.GetAllAsync<Board>();
-
-            return new Box
-            {
-                Boards = await boards,
-                Cards = await cards,
-                Columns = await columns,
-                Rows = await rows
-            };
-        }
-
-        public async Task<BoxScheme> LoadScheme()
-        {
-            var rows = db.GetAllAsync<Row>();
-            var columns = db.GetAllAsync<Column>();
-            var boards = db.GetAllAsync<Board>();
-
-            return new BoxScheme
-            {
-                Boards = await boards,
-                Columns = await columns,
-                Rows = await rows
-            };
-        }
-
-        public Task<List<Board>> LoadSchemeBoards()
-        {
-            return db.GetAllAsync<Board>();
-        }
-
-        public async Task<List<Column>> LoadSchemeColumns(int[] boardIds = null)
-        {
-            var columns = await db.GetAllAsync<Column>();
-            return columns
-                .Where(x => boardIds?.Contains(x.BoardId) ?? true)
-                .ToList();
-        }
-
-        public async Task<List<Row>> LoadSchemeRows(int[] boardIds = null)
-        {
-            var rows = await db.GetAllAsync<Row>();
-            return rows
-                .Where(x => boardIds?.Contains(x.BoardId) ?? true)
-                .ToList();
-        }
-
-        public async Task<List<Card>> LoadCards(CardFilter filter)
-        {
-            var cards = await db.GetAllAsync<Card>();
-            if (filter.IsEmpty)
-                return cards;
-
-            var filteredCards =
-                from card in cards
-                join boardId in filter.BoardIds on card.BoardId equals boardId
-                join columnId in filter.ColumnIds on card.ColumnId equals columnId
-                join rowId in filter.RowIds on card.ColumnId equals rowId
-                select card;
-
-            return filteredCards.ToList();
-        }
-
-        public Task<Row> CreateOrUpdateRow(Row row)
-        {
-            return db.UpsertAsync(row);
-        }
-
-        public Task<Column> CreateOrUpdateColumn(Column column)
-        {
-            return db.UpsertAsync(column);
-        }
-
-        public Task<Card> CreateOrUpdateCard(Card card)
-        {
-            return db.UpsertAsync(card);
-        }
-
-        public Task<Board> CreateOrUpdateBoard(Board board)
-        {
-            return db.UpsertAsync(board);
-        }
-
-        public Task DeleteRow(int rowId)
-        {
-            return Task.Run(() =>
-            {
-                var rows = db.GetCollectionByType<Row>();
-                rows.DeleteMany(x => x.Id == rowId);
-            });
-        }
-
-        public Task DeleteColumn(int columnId)
-        {
-            return Task.Run(() =>
-            {
-                var columns = db.GetCollectionByType<Column>();
-                columns.DeleteMany(x => x.Id == columnId);
-            });
-        }
-
-        public Task DeleteCard(int cardId)
-        {
-            return Task.Run(() =>
-            {
-                var cards = db.GetCollectionByType<Card>();
-                cards.DeleteMany(x => x.Id == cardId);
-            });
-        }
-
-        public Task DeleteBoard(int boardId)
-        {
-            return Task.Run(() =>
-            {
-                var boards = db.GetCollectionByType<Board>();
-                boards.DeleteMany(x => x.Id == boardId);
-            });
+            manager = new LiteDbManager(uri);
         }
 
         public void Dispose()
         {
-            db?.Dispose();
+            manager.CloseDb();
         }
+
+        public async Task<Box> Load()
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                var cards = db.GetAllAsync<Card>();
+                var rows = db.GetAllAsync<Row>();
+                var columns = db.GetAllAsync<Column>();
+                var boards = db.GetAllAsync<Board>();
+
+                return new Box
+                {
+                    Boards = await boards,
+                    Cards = await cards,
+                    Columns = await columns,
+                    Rows = await rows
+                };
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public async Task<BoxScheme> LoadScheme()
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                var rows = db.GetAllAsync<Row>();
+                var columns = db.GetAllAsync<Column>();
+                var boards = db.GetAllAsync<Board>();
+
+                return new BoxScheme
+                {
+                    Boards = await boards,
+                    Columns = await columns,
+                    Rows = await rows
+                };
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task<List<Board>> LoadSchemeBoards()
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return db.GetAllAsync<Board>();
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public async Task<List<Column>> LoadSchemeColumns(int[] boardIds = null)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                var columns = await db.GetAllAsync<Column>();
+                return columns
+                    .Where(x => boardIds?.Contains(x.BoardId) ?? true)
+                    .ToList();
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public async Task<List<Row>> LoadSchemeRows(int[] boardIds = null)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                var rows = await db.GetAllAsync<Row>();
+                return rows
+                    .Where(x => boardIds?.Contains(x.BoardId) ?? true)
+                    .ToList();
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public async Task<List<Card>> LoadCards(CardFilter filter)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                var cards = await db.GetAllAsync<Card>();
+                if (filter.IsEmpty)
+                    return cards;
+
+                var filteredCards =
+                    from card in cards
+                    join boardId in filter.BoardIds on card.BoardId equals boardId
+                    join columnId in filter.ColumnIds on card.ColumnId equals columnId
+                    join rowId in filter.RowIds on card.ColumnId equals rowId
+                    select card;
+
+                return filteredCards.ToList();
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task<Row> CreateOrUpdateRow(Row row)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return db.UpsertAsync(row);
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task<Column> CreateOrUpdateColumn(Column column)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return db.UpsertAsync(column);
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task<Card> CreateOrUpdateCard(Card card)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return db.UpsertAsync(card);
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task<Board> CreateOrUpdateBoard(Board board)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return db.UpsertAsync(board);
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task DeleteRow(int rowId)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return Task.Run(() =>
+                {
+                    var rows = db.GetCollectionByType<Row>();
+                    rows.DeleteMany(x => x.Id == rowId);
+                });
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task DeleteColumn(int columnId)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return Task.Run(() =>
+                {
+                    var columns = db.GetCollectionByType<Column>();
+                    columns.DeleteMany(x => x.Id == columnId);
+                });
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task DeleteCard(int cardId)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return Task.Run(() =>
+                {
+                    var cards = db.GetCollectionByType<Card>();
+                    cards.DeleteMany(x => x.Id == cardId);
+                });
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
+        public Task DeleteBoard(int boardId)
+        {
+            var db = manager.LockDb();
+
+            try
+            {
+                return Task.Run(() =>
+                {
+                    var boards = db.GetCollectionByType<Board>();
+                    boards.DeleteMany(x => x.Id == boardId);
+                });
+            }
+            finally
+            {
+                manager.FreeDb();
+            }
+        }
+
     } //end of class
 }
